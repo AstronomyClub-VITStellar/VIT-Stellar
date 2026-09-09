06b68e0f358b1644cb5f82f6dac5a7dbbbc3dbb1
Astronomy Club - VIT Stellar <haneeshrao619@gmail.com>
2026-09-09 13:11:13 +0530

    Testing

--- src/database/Deadlines.js
+++ src/database/Deadlines.js
@@ -5,7 +5,7 @@ export const merchandiseDeadlines = {
 
 export const festDeadlines = {
   shotsUploadDate:    new Date('2026-09-05T09:00:00+05:30'),
-  teamRegOpenDate:    new Date('2026-09-08T20:00:00+05:30'),
+  teamRegOpenDate:    new Date('2026-09-11T20:00:00+05:30'),
   teamRegClosingDate: new Date('2026-09-12T11:59:59+05:30'),
 };
 
